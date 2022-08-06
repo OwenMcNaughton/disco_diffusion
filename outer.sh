849e1057bec31ccf7cde6615bb6b9a7941d63dfe
Owen McNaughton <owen.mcnaughton@gmail.com>
2022-08-06 18:10:24 +0100

    Create outer.sh

---
+++ outer.sh
@@ -0,0 +1,2 @@
+python3 mdd.py camus_1 3 camus 100 50 101 "black and white 4k illustration of a cybernetic computer, trending on artstation:10" "dof:-1" "blur:-1"
+python3 mdd.py camus_1 3 camus 200 100 101 "black and white 4k illustration of a cybernetic computer, trending on artstation:10" "dof:-1" "blur:-1"
